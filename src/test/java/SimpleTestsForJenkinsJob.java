0ed1c7c9353d109026c1c63eede4dcfaeaa703a2
KoninklijkeSev <strong.m.m@mail.ru>
2021-02-22 17:08:53 +0300

    Fix bags

--- src/test/java/SimpleTestsForJenkinsJob.java
+++ src/test/java/SimpleTestsForJenkinsJob.java
@@ -1,11 +1,9 @@
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static io.qameta.allure.Allure.step;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.is;
 
 public class SimpleTestsForJenkinsJob {
     @Test
@@ -16,11 +14,9 @@ public class SimpleTestsForJenkinsJob {
     }
 
     @Test
-    @Tag("negative")
     void negativeTest() {
         assertEquals(true, false);
     }
-
     @Test
     @Tag("positive")
     void successWithHamcrestTest() {
@@ -28,24 +24,7 @@ public class SimpleTestsForJenkinsJob {
     }
 
     @Test
-    @Tag("negative")
     void negativeWithHamcrestTest() {
         assertThat(true, is(false));
     }
-
-    @Test
-    @Tag("positive")
-    @DisplayName("Some positive test")
-    void successWithHamcrestAndStepsTest() {
-        step("Assert that true is true", () ->
-                assertThat(true, is(true)));
     }
-
-    @Test
-    @Tag("negative")
-    @DisplayName("Some negative test")
-    void negativeWithHamcrestAndStepsTest() {
-        step("Assert that true is false", () ->
-                assertThat(true, is(false)));
-    }
-}
